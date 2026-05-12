6e07b5b15bad7b8fcd0295cde1aa180ed57b343b
PraveenKumar <preethamkatariya@gmail.com>
2026-05-12 12:01:47 +0530

    fix: cors and port for vercel

--- backend/server.js
+++ backend/server.js
@@ -9,31 +9,29 @@ const issueRoutes = require("./routes/issueRoutes");
 
 const app = express();
 
-
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://fsd-03-issue-tracking-system-og35.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
-
 
 // Routes
 app.use("/api/auth", authRoutes);
-
 app.use("/api/issues", issueRoutes);
-
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-
 // Test Route
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
